92efe2f7926a44e86bd59aa2243e00a5aea22a98
pablocontreras <pablo_contreras_1997@outlook.com>
2018-11-28 01:44:11 -0600

    La vie en rose

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,7 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-
+    <link rel="stylesheet" type="text/css" href="{{asset('/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/css/bootstrap.css')}}">
 
     <style type="text/css">
       #footer{
@@ -27,7 +28,7 @@
     </style>
   </head>
   <body>
-      <nav class="navbar navbar-dark bg-dark navbar-expand-sm fixed-bottom">
+      <nav class="navbar navbar-dark bg-dark navbar-expand-sm fixed-bottom" >
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#topNav">
             <span class="navbar-toggler-icon"></span>
         </button>
